030c718bb5f21e58c88c22e9fb2104dee387d612
Harrison Ting <runhappy@gmail.com>
2017-07-14 01:05:51 -0700

    get_spectator_game_info and error handling

--- spectate.py
+++ spectate.py
@@ -1,14 +1,46 @@
+from typing import Dict
+
 import requests
 
 # From https://developer.riotgames.com/
+_PLATFORM_ID = 'NA1'
+_SUMMONER_NAME = 'Rioting'
 _API_KEY = 'RGAPI-70214411-99f0-4b0f-a4d5-2afa63f09a6a'
 
 
-def _spectate():
-    result = requests.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Rioting', params={'api_key': _API_KEY})
+class SummonerDoesNotExist(Exception):
+    pass
+
+
+def _get_summoner_id() -> int:
+    result = requests.get(f'https://{_PLATFORM_ID}.api.riotgames.com/lol/summoner/v3/summoners/by-name/{_SUMMONER_NAME}', params={'api_key': _API_KEY})
+    if result.status_code == 404:
+        raise SummonerDoesNotExist(f'No summoner with name {_SUMMONER_NAME} found on platform {_PLATFORM_ID}')
     result.raise_for_status()
     data = result.json()
-    print(data)
+    return data['id']
+
+
+class NotInGameError(Exception):
+    pass
+
+
+def _get_spectator_game_info(summoner_id: int) -> Dict:
+    result = requests.get(f'https://{_PLATFORM_ID}.api.riotgames.com/observer-mode/rest/consumer/getSpectatorGameInfo/{_PLATFORM_ID}/{summoner_id}', params={'api_key': _API_KEY})
+    if result.status_code == 404:
+        raise NotInGameError(f'No summoner with ID {summoner_id} found on platform {_PLATFORM_ID}')
+    result.raise_for_status()
+    data = result.json()
+    return data
+
+
+def _spectate():
+    summoner_id = _get_summoner_id()
+    try:
+        data = _get_spectator_game_info(summoner_id)
+        print(data)
+    except NotInGameError:
+        print('Not in game')
 
 
 def _main():
